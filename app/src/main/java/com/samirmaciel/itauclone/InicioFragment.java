87f273142c8b0fae21aca198e319e0052547b7c2
Samir Maciel <samir.maciel17@gmail.com>
2021-04-08 21:20:56 -0300

    tela home - criando a scrollView2

--- app/src/main/java/com/samirmaciel/itauclone/InicioFragment.java
+++ app/src/main/java/com/samirmaciel/itauclone/InicioFragment.java
@@ -6,17 +6,21 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.transition.AutoTransition;
+import android.transition.Slide;
 import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Scroller;
 
 
 public class InicioFragment extends Fragment {
 
     private ImageView iconExpandir;
 
+    private LinearLayout linearLayoutContainer;
     private CardView cardViewSaldo;
     private CardView cardViewValorSaldo;
 
@@ -35,19 +39,20 @@ public class InicioFragment extends Fragment {
         iconExpandir = (ImageView) view.findViewById(R.id.iconSetaExpandir);
         cardViewSaldo = (CardView) view.findViewById(R.id.cardViewSaldo);
         cardViewValorSaldo = (CardView) view.findViewById(R.id.cardViewValorSaldo);
+        linearLayoutContainer = (LinearLayout) view.findViewById(R.id.linearLayoutContainer);
 
         iconExpandir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(cardViewValorSaldo.getVisibility() == View.GONE){
-                    TransitionManager.beginDelayedTransition(cardViewSaldo, new AutoTransition());
+                    TransitionManager.beginDelayedTransition(linearLayoutContainer, new AutoTransition());
                     iconExpandir.animate().rotationX(180f).start();
                     cardViewValorSaldo.setVisibility(View.VISIBLE);
 
                 }else{
-                    TransitionManager.beginDelayedTransition(cardViewSaldo, new AutoTransition());
-                    iconExpandir.animate().rotationX(0f).start();
+                    TransitionManager.beginDelayedTransition(linearLayoutContainer, new AutoTransition());
                     cardViewValorSaldo.setVisibility(View.GONE);
+                    iconExpandir.animate().rotationX(0f).start();
                 }
             }
         });
